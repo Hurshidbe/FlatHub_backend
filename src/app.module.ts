a4fb00d7ff85f48931b3bea740cad75f00399183
hurshidbe <egamberdiyevx8@gmail.com>
2025-09-20 17:24:00 +0500

    user model created

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LessorsModule } from './modules/lessors/lessors.module';
-import { RentersModule } from './modules/renters/renters.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [LessorsModule, RentersModule],
+  imports: [LessorsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
